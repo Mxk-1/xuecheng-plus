dcfe5864394506ce506b851b38c8464612b4c298
maxinkai <irvingmxk@163.com>
2023-10-25 20:21:19 +0800

    添加部分Swagger注解，添加时间转换

--- xuecheng-plus-content/xuecheng-plus-content-api/src/main/java/com/xuecheng/ContentApplication.java
+++ xuecheng-plus-content/xuecheng-plus-content-api/src/main/java/com/xuecheng/ContentApplication.java
@@ -1,5 +1,6 @@
 package com.xuecheng;
 
+import com.spring4all.swagger.EnableSwagger2Doc;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -8,6 +9,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  * @date 2023/10/25 19:03
  **/
 @SpringBootApplication
+@EnableSwagger2Doc
 public class ContentApplication {
     public static void main(String[] args) {
         SpringApplication.run(ContentApplication.class, args);
--- xuecheng-plus-content/xuecheng-plus-content-api/src/main/java/com/xuecheng/content/api/CourseBaseInfoController.java
+++ xuecheng-plus-content/xuecheng-plus-content-api/src/main/java/com/xuecheng/content/api/CourseBaseInfoController.java
@@ -4,6 +4,9 @@ import com.xuecheng.base.model.PageParams;
 import com.xuecheng.base.model.PageResult;
 import com.xuecheng.content.model.dto.QueryCourseParamsDto;
 import com.xuecheng.content.model.po.CourseBase;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -15,8 +18,10 @@ import org.springframework.web.bind.annotation.RestController;
  * @date 2023/10/25 18:50
  **/
 @RestController
+@Api(tags = "课程信息编辑接口", value = "课程信息编辑接口")
 public class CourseBaseInfoController {
-    @RequestMapping("/course/list")
+    @PostMapping("/course/list")
+    @ApiOperation("课程查询接口")
     public PageResult<CourseBase> list(PageParams pageParams, @RequestBody(required = false) QueryCourseParamsDto queryCourseParamsDto) {
         return null;
     }
