4e9659c1fe4ff1494550a947d3a470a682e0bc57
maxinkai <irvingmxk@163.com>
2023-10-27 18:36:49 +0800

    搭建gateway网关

---
+++ xuecheng-plus-gateway/src/main/java/com/xuecheng/GatewayApplication.java
@@ -0,0 +1,16 @@
+package com.xuecheng;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author maxinkai
+ * @description
+ * @date 2023/10/27 18:25
+ **/
+@SpringBootApplication
+public class GatewayApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(GatewayApplication.class, args);
+    }
+}
