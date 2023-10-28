1bc37c9e7b610e181b4564e58aadc23278869825
maxinkai <irvingmxk@163.com>
2023-10-28 17:31:58 +0800

    通过minio上传文件

---
+++ xuecheng-plus-media/xuecheng-plus-media-model/src/main/java/com/xuecheng/media/model/dto/UploadFileResultDto.java
@@ -0,0 +1,14 @@
+package com.xuecheng.media.model.dto;
+
+import com.xuecheng.media.model.po.MediaFiles;
+import lombok.Data;
+
+/**
+ * @author maxinkai
+ * @description
+ * @date 2023/10/28 16:05
+ **/
+@Data
+public class UploadFileResultDto extends MediaFiles {
+
+}
